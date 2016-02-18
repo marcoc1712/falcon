c9e3336ac73960364d00bdb039018976e66d3e12
marco <marco@pc-ubuntu>
2016-02-18 16:45:08 +0100

    Initial commit

--- falcon/resources/install/debian_ea/configApache2.sh
+++ falcon/resources/install/debian_ea/configApache2.sh
@@ -35,10 +35,10 @@ function run_as_root() {
 run_as_root	# run this first!
 
 # copia la configurazione del webserver Apache2
-#mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default-old.conf
-#cp /var/www/falcon/falcon/resources/install/WebServer/apache2/etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf
-#abilita le CGI
+mv /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default-old.conf
+cp /var/www/falcon/falcon/resources/install/WebServer/apache2/etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf
 
+#abilita le CGI
 ln -s /etc/apache2/mods-available/cgid.conf /etc/apache2/mods-enabled/cgid.conf
 ln -s /etc/apache2/mods-available/cgid.load /etc/apache2/mods-enabled/cgid.load
 
