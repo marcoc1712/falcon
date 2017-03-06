34d2b236b184b0b162709385f4e32bbcb2a811d6
marco <marco@192.168.56.1>
2017-03-06 12:22:45 +0100

    first added testmodule

---
+++ falcon/src/test.pl
@@ -0,0 +1,41 @@
+
+#!/usr/bin/perl
+# $Id$
+#
+# WEB INTERFACE and Controll application for an headless squeezelite
+# installation.
+#
+# Best used with Squeezelite-R2 
+# (https://github.com/marcoc1712/squeezelite/releases)
+#
+# Copyright 2016 Marco Curti, marcoc1712 at gmail dot com.
+# Please visit www.marcoc1712.it
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License,
+# version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+################################################################################
+
+use strict;
+use warnings;
+use utf8;
+
+use Data::Dump qw(dump pp);
+use FindBin qw($Bin);
+use lib $Bin;
+use lib "../lib";
+
+use Log::Log4perl;
+use WebInterface::Status;
+
+my $log;
+
+my $status= WebInterface::Status->new();
+
+dump $status->getStatus();
