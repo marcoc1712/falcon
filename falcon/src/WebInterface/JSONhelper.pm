b1f9c343f49f46e8f1df17b40553a6adb385f21b
marco <marco@192.168.56.1>
2017-03-16 03:59:08 +0100

    load dsdFormats

--- falcon/src/WebInterface/JSONhelper.pm
+++ falcon/src/WebInterface/JSONhelper.pm
@@ -51,7 +51,7 @@ sub decode{
     
     my $out;
 
-    if (! $json_txt || $json_txt eq '') {return undef;}
+    if (! $json_txt && !$json_txt eq '') {return undef;}
     my $data = decode_json $json_txt;
     
     return $data;
